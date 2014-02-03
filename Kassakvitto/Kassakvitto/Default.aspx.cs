487ae152d9117299ce6e710ac9f7d53e8cf0565d
fredrikblixth <fb222fn@student.lnu.se>
2014-02-03 22:47:36 +0100

    Labb2
    Färdig

--- Kassakvitto/Kassakvitto/Default.aspx.cs
+++ Kassakvitto/Kassakvitto/Default.aspx.cs
@@ -2,11 +2,6 @@
 {
     using Kassakvitto.Model;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Web;
-    using System.Web.UI;
-    using System.Web.UI.WebControls;
 
     public partial class Default : System.Web.UI.Page
     {
